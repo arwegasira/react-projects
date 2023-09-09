4443875f9b97d6b9ee4dc24e56fb99620cff5fda
arwegasira <ac.rwegasira@gmail.com>
2023-09-09 09:09:18 +0200

    sidebar sublinks

---
+++ 1-navbar/src/SidebarLinks.jsx
@@ -0,0 +1,37 @@
+import subLinks from './data'
+import { useGlobalContext } from './Context'
+const SidebarLinks = () => {
+  const { pageId, submenuTop, submenuLeft, setPageId } = useGlobalContext()
+  const currentPage = subLinks.find((item) => item.pageId === pageId)
+
+  const handleMouseLeave = (e) => {
+    const { clientX, clientY } = e
+    const { left, right, bottom } = e.target.getBoundingClientRect()
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null)
+    }
+  }
+
+  return (
+    <div
+      className={pageId ? 'sidebarLinks show' : 'sidebarLinks'}
+      style={{ top: submenuTop, left: submenuLeft }}
+      onMouseLeave={(e) => handleMouseLeave(e)}
+    >
+      <div className='sidebarLinks-center'>
+        <h5>{currentPage?.page}</h5>
+        <div className='sidebarLinks-links'>
+          {currentPage?.links?.map((link) => {
+            const { label, icon, url, id } = link
+            return (
+              <a href={url} key={id}>
+                {icon} {label}
+              </a>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+export default SidebarLinks
